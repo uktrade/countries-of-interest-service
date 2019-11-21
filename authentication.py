78f90bdea7dac3d4fa37dd3a87bf9c43531e3a2b
David Voong <voong.david@gmail.com>
2019-11-21 16:38:21 +0000

    add nonce authentication

--- authentication.py
+++ authentication.py
@@ -15,7 +15,8 @@ def hawk_authenticate():
         url=url,
         method=method,
         content=content,
-        content_type=content_type
+        content_type=content_type,
+        seen_nonce=seen_nonce,
     )
 
 def hawk_decorator_factory(hawk_enabled):
@@ -23,6 +24,16 @@ def hawk_decorator_factory(hawk_enabled):
         return hawk_required
     else:
         return lambda view, *args, **kwargs: view
+
+def hawk_required(view, *args, **kwargs):
+    def wrapper(*args, **kwargs):
+        try:
+            hawk_authenticate()
+        except Exception:
+            return 'Hawk authentication failed', 401
+        return view(*args, **kwargs)
+    wrapper.__name__ = view.__name__
+    return wrapper
 
 def lookup_hawk_credentials(client_id):
     sql = 'select * from users where client_id=%s'
@@ -39,12 +50,24 @@ def lookup_hawk_credentials(client_id):
         'algorithm': 'sha256'
     }
 
-def hawk_required(view, *args, **kwargs):
-    def wrapper(*args, **kwargs):
-        try:
-            hawk_authenticate()
-        except Exception:
-            return 'Hawk authentication failed', 401
-        return view(*args, **kwargs)
-    wrapper.__name__ = view.__name__
-    return wrapper
+def seen_nonce(client_id, nonce, timestamp):
+    with get_db() as connection:
+        sql = 'create table if not exists hawk_nonce ' \
+            '(client_id varchar(100), nonce varchar(100), timestamp int)'
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+
+    with get_db() as connection:
+        sql = 'select * from hawk_nonce where client_id=%s and nonce=%s and timestamp=%s'
+        with connection.cursor() as cursor:
+            cursor.execute(sql, [client_id, nonce, timestamp])
+            seen = len(cursor.fetchall()) > 0
+
+    if seen is True:
+        return True
+    else:
+        with get_db() as connection:
+            sql = 'insert into hawk_nonce values (%s, %s, %s)'
+            with connection.cursor() as cursor:
+                cursor.execute(sql, [client_id, nonce, timestamp])
+        return False
