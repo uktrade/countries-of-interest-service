339f8533b72607d883dec10d3edca38d574ddee6
David Voong <voong.david@gmail.com>
2019-12-24 12:27:34 +0000

    removed data report and refactored core tasks

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,10 +1,10 @@
 import celery
 
 import app.application
-import app.etl.tasks.core
+import app.etl.tasks
 
 
 @celery.task(ignore_result=True)
 def populate_database_task(drop_table=True):
     with app.application.get_or_create().app_context():
-        return app.etl.tasks.core.populate_database(drop_table)
+        return app.etl.tasks.populate_database(drop_table)
