f079962cf479a201796deba1ad8ad35c194bdf1c
Kristof Geebelen <geebelenk@gmail.com>
2019-12-30 12:05:25 +0000

    datahub interaction coi extraction

--- tests/fixtures/add_to_db.py
+++ tests/fixtures/add_to_db.py
@@ -6,6 +6,7 @@ from app.db.models import (
     DITCountryTerritoryRegister,
     DatahubCompanyIDToCompaniesHouseCompanyNumber,
     ExportCountries,
+    Interactions,
     SectorsOfInterest,
 )
 
@@ -113,6 +114,29 @@ def add_country_territory_registry(app_with_db_module):
                 'type': record.get('type', None),
             }
             DITCountryTerritoryRegister.get_or_create(
+                id=record.get('id', None), defaults=defaults,
+            )
+
+    return _method
+
+
+@pytest.fixture(scope='module')
+def add_datahub_interaction(app_with_db_module):
+    def _method(records):
+        for record in records:
+            defaults = {
+                'datahub_id': record.get('datahub_id', None),
+                'companies_house_number': record.get('companies_house_number', None),
+                'subject': record.get('subject', None),
+                'policy_feedback_notes': record.get('policy_feedback_notes', None),
+                'notes': record.get('notes', None),
+                'source': record.get('source', None),
+                'source_id': record.get('source_id', None),
+                'created_on': record.get('created_on', None),
+                'country_name': record.get('country_name', None),
+                'country_alpha2': record.get('country_alpha2', None),
+            }
+            Interactions.get_or_create(
                 id=record.get('id', None), defaults=defaults,
             )
 
