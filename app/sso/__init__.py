a6668ebaf94fd03a9ac88ddd76914ce63eb71a4b
David Voong <david.voong@digital.trade.gov.uk>
2020-01-07 15:02:15 +0000

    monkey patch urllib to specify cafile for oauth (#48)
    * monkey patch urllib to specify cafile for oauth
    * fix sectors_of_interest task

--- app/sso/__init__.py
+++ app/sso/__init__.py
@@ -1,12 +1,23 @@
+import urllib.request
+
 import certifi
 
 from flask import Blueprint, redirect, request, session, url_for
 
 from flask_oauthlib.client import OAuth
 
-
 import werkzeug
 from werkzeug.exceptions import abort
+
+
+original_open = urllib.request.urlopen
+
+
+def myopen(path):
+    return original_open(path, cafile=certifi.where())
+
+
+urllib.request.urlopen = myopen
 
 
 sso = Blueprint('sso', __name__)
