af34e97d6cc20431875bbfaa1d2525627df55da3
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2020-01-09 12:33:03 +0000

    Re-enable scheduler

--- app/etl/scheduler.py
+++ app/etl/scheduler.py
@@ -7,7 +7,7 @@ class Scheduler:
     def __init__(self, populate_database_task):
         self.scheduler = BackgroundScheduler()
         self.scheduler.add_job(
-            populate_database_task.delay, 'cron', hour='0',
+            populate_database_task, 'cron', hour='0',
         )
         atexit.register(lambda: self.scheduler.shutdown(wait=False))
 
--- app/worker.py
+++ app/worker.py
@@ -1,4 +1,9 @@
 from app.application import get_or_create
+from app.etl.scheduler import Scheduler
+from app.api.tasks import populate_database_task
 
 app = get_or_create()
 celery_app = app.celery
+
+scheduled_task = Scheduler(populate_database_task)
+scheduled_task.start()
