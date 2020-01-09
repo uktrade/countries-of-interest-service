b62db4457ee351891af7d761b15d85728f5ddded
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2020-01-09 12:44:55 +0000

    Obey config setting

--- app/worker.py
+++ app/worker.py
@@ -1,9 +1,10 @@
+from app.api.tasks import populate_database_task
 from app.application import get_or_create
 from app.etl.scheduler import Scheduler
-from app.api.tasks import populate_database_task
 
 app = get_or_create()
 celery_app = app.celery
 
-scheduled_task = Scheduler(populate_database_task)
-scheduled_task.start()
+if app.config['app']['run_scheduler']:
+    scheduled_task = Scheduler(populate_database_task)
+    scheduled_task.start()
