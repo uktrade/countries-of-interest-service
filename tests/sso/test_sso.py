eaf722410d5231529b96454d928609c1c8a638a3
David Voong <voong.david@gmail.com>
2020-02-04 12:07:17 +0000

    Add unit tests

---
+++ tests/sso/test_sso.py
@@ -0,0 +1,180 @@
+import unittest.mock
+
+from flask import session
+import pytest
+
+import app.sso as sso
+import app.sso.token as token
+
+
+@pytest.fixture(scope="function")
+def sso_authenticated_request():
+    with unittest.mock.patch('app.sso.token.is_authenticated') as mock_is_authenticated:
+        mock_is_authenticated.return_value = True
+        yield
+
+@pytest.fixture(scope="function")
+def sso_client(app):
+    kwargs = {
+        'access_token_url': 'access_token_url',
+        'app': app,
+        'authorize_url': 'authorize_url',
+        'client_id': 'client_id',
+        'client_secret': 'client_secret',
+        'logout_url': 'logout_url',
+        'profile_url': 'profile_url',
+        'sso_session_token_key': 'sso_session_token_key',
+        'sso_url': 'sso_url',
+        'user_datastore': 'user_datastore',
+    }
+
+    client = sso.BaseSSOClient(**kwargs)
+    client.process_login = lambda response: None
+    client.process_logout = lambda: None
+    
+    return client
+
+@pytest.fixture(scope="function")
+def mock_oauth():
+    with unittest.mock.patch('app.sso.OAuth') as mock_oauth:
+        yield mock_oauth().remote_app()
+
+@pytest.fixture(scope="module")
+def mock_url_for():
+    with unittest.mock.patch('app.sso.url_for') as mock_url_for:
+        yield mock_url_for
+
+class TestInit:
+
+    def test_sets_attributes(self, app, mock_oauth, sso_client):
+        assert sso_client.sso_session_token_key == 'sso_session_token_key'
+        assert sso_client.user_datastore == 'user_datastore'
+        assert sso_client.db == app.db
+        assert sso_client.profile_url == 'profile_url'
+        assert sso_client.logout_url == 'logout_url'
+        assert sso_client.oauth_broker == mock_oauth
+
+class TestLogin:
+
+    def test_returns_oauth_broker_authorize(
+            self,
+            app,
+            mock_oauth,
+            mock_url_for,
+            sso_client
+    ):
+        response = sso_client.login()
+        mock_oauth.authorize.assert_called_once_with(
+            callback=mock_url_for('sso.callback', _external=True)
+        )
+
+class TestLogout:
+
+    def test_removes_session_key(self, app, sso_client):
+        with app.test_request_context() as r:
+            session['sso_session_token_key'] = ('access_token', '')
+            
+        with app.test_client() as c:
+            response = c.get('/logout')
+            
+        with app.test_request_context() as r:
+            assert len(session) == 0
+
+    def test_redirect(self, app, sso_client):
+        with app.test_request_context() as r:
+            response = sso_client.logout()
+        assert response.status_code == 302
+        assert response.location == 'logout_url'
+
+
+class TestCallback:
+
+    def test_no_access_token_provided(self, app, sso_client):
+        with app.test_request_context(
+                '/?error=error&error_description=description'
+        ) as request:
+            response = sso_client.callback()
+            print('session:', session)
+            assert 'sso_session_token_key' not in session
+
+    def test_adds_token_to_session(self, app, mock_oauth, sso_client):
+        response = {'access_token': 'access_token'}
+        mock_oauth.authorized_response.return_value = response
+        sso_client.oauth_broker = mock_oauth
+        with app.test_request_context() as request:
+            response = sso_client.callback()
+            assert session['sso_session_token_key'] == ('access_token', '')
+
+
+class TestGetProfile:
+
+    def test_get_profile(self, app, mock_oauth, sso_client):
+
+        mock_response = unittest.mock.Mock()
+        mock_response.status = 200
+        mock_response.data = {"name": "me"}
+        mock_oauth.get.return_value = mock_response
+        sso_client.oauth_broker = mock_oauth
+        
+        with app.test_request_context() as request:
+            response = sso_client.get_profile()
+
+        assert response == mock_response.data
+
+
+class TestGetNextUrl:
+
+    def test_if_next_in_request(self, app, sso_client):
+        with app.test_request_context('/?next=/apples'):
+            response = sso_client._get_next_url()
+        assert response == '/apples'
+
+    def test_if_next_in_session(self, app, sso_client):
+        with app.test_request_context():
+            session['next'] = '/oranges'
+            response = sso_client._get_next_url()
+        assert response == '/oranges'
+
+    def test_next_not_specified(self, app, sso_client):
+        with app.test_request_context():
+            response = sso_client._get_next_url()
+        assert response == '/'
+
+    def test_next_specified_in_request_and_session(self, app, sso_client):
+        with app.test_request_context('/?next=/apples'):
+            session['next'] = '/oranges'
+            response = sso_client._get_next_url()
+        assert response == '/apples'
+
+
+def test_authenticated_view(app, sso_authenticated_request):
+
+    import app.api.views as views
+    from flask.blueprints import Blueprint
+
+    test_sso = Blueprint(
+        name='tests.sso.test_sso_2.test_authenticated_view',
+        import_name=__name__
+    )
+    @token.login_required
+    @test_sso.route('/test-view')
+    def test_view():
+        return 'success'
+    app.register_blueprint(test_sso)
+
+    with app.test_client() as c:
+        response = c.get('/test-view')
+
+def test_authenticated_view_2(app, sso_authenticated_request):
+
+    @token.login_required
+    def test_view():
+        return 'success'
+
+    with app.test_request_context():
+        response = test_view()
+        assert response == 'success'
+
+
+
+    
---
+++ tests/sso/test_token.py
@@ -0,0 +1,87 @@
+import unittest
+import unittest.mock
+
+import pytest
+from flask_oauthlib.client import OAuthException
+from urllib.error import URLError
+from werkzeug.exceptions import HTTPException
+
+import app.sso.token as token
+
+
+@pytest.fixture
+def sso_client(app):
+    kwargs = {
+        'access_token_url': 'access_token_url',
+        'app': app,
+        'authorize_url': 'authorize_url',
+        'client_id': 'client_id',
+        'client_secret': 'client_secret',
+        'logout_url': 'logout_url',
+        'profile_url': 'profile_url',
+        'sso_session_token_key': 'sso_session_token_key',
+        'sso_url': 'sso_url',
+        'user_datastore': 'user_datastore',
+    }
+
+    return token.SSOClient(**kwargs)
+
+
+class TestSSoClient:
+    def test_process_login(self, sso_client):
+        sso_response = unittest.mock.Mock()
+        response = sso_client.process_login(sso_response)
+        assert response is None
+
+    def test_process_logout(self, sso_client):
+        response = sso_client.process_logout()
+        assert response is None
+
+
+class TestLoginRequired:
+    @unittest.mock.patch('app.sso.token.is_authenticated')
+    def test_if_authenticated_return_view(self, is_authenticated):
+        is_authenticated.return_value = True
+        view = lambda: "view"
+        wrapped_view = token.login_required(view)
+        assert view() == wrapped_view()
+
+    @unittest.mock.patch('app.sso.token.is_authenticated')
+    @unittest.mock.patch('app.sso.token.redirect')
+    @unittest.mock.patch('app.sso.token.request')
+    @unittest.mock.patch('app.sso.token.url_for')
+    def test_if_authenticated_return_view(
+        self, url_for, request, redirect, is_authenticated
+    ):
+        is_authenticated.return_value = False
+        view = lambda: "view"
+        wrapped_view = token.login_required(view)
+        redirect.asser_called_once()
+        response = wrapped_view()
+
+
+class TestIsAuthenticated:
+    def test_if_get_profile_does_not_error_return_true(self, app):
+        from flask import current_app as flask_app
+
+        flask_app.sso_client = unittest.mock.Mock()
+        response = token.is_authenticated()
+        assert response == True
+
+    def test_if_get_profile_error_is_caught_return_false(self, app):
+        from flask import current_app as flask_app
+
+        flask_app.sso_client = unittest.mock.Mock()
+        handled_exceptions = [URLError('e'), OAuthException('e'), HTTPException('e')]
+        flask_app.sso_client.get_profile.side_effect = handled_exceptions
+        for e in handled_exceptions:
+            response = token.is_authenticated()
+            assert response == False
+
+    def test_if_unhandled_exception_raise_exception(self, app):
+        from flask import current_app as flask_app
+
+        flask_app.sso_client = unittest.mock.Mock()
+        flask_app.sso_client.get_profile.side_effect = Exception('unhandled')
+        with pytest.raises(Exception):
+            response = token.is_authenticated()
