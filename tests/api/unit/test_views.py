32413af2b36d919d6a09ce4ba8ec28745806dcd8
David Voong <voong.david@gmail.com>
2020-04-28 13:20:23 +0100

    fix tests

--- tests/api/unit/test_views.py
+++ tests/api/unit/test_views.py
@@ -1,30 +1,45 @@
 import datetime
 import unittest.mock
 
+import pytest
 import redis
 from data_engineering.common.views import get_client_scope, json_error, seen_nonce
 from werkzeug.exceptions import BadRequest, NotFound
 
 
-class TestGetClientScope(unittest.TestCase):
-    @unittest.mock.patch('data_engineering.common.db.models.HawkUsers')
-    def test_if_client_scope_is_not_found_return_error(self, HawkUsers):
-        client_id = 0
+@pytest.fixture
+def HawkUsers():
+    path = 'data_engineering.common.db.models.HawkUsers'
+    with unittest.mock.patch(path) as mock_obj:
+        yield mock_obj
+
+
+@pytest.fixture
+def logging():
+    path = 'data_engineering.common.views.flask_app.logger'
+    with unittest.mock.patch(path) as mock_obj:
+        yield mock_obj
+
+
+class TestGetClientScope:
+    def test_if_client_scope_is_not_found_return_error(self, app_with_db, HawkUsers):
+        client_id = 'client_id'
         HawkUsers.get_client_scope.return_value = None
-        with self.assertRaises(LookupError):
+        with pytest.raises(LookupError):
             get_client_scope(client_id)
 
 
-class TestSeenNonce(unittest.TestCase):
-    @unittest.mock.patch('app.api.views.flask_app.logger')
-    @unittest.mock.patch('app.api.views.flask_app')
-    def test_if_there_is_a_redis_error_assume_nonce_has_been_seen(self, app, logging):
+class TestSeenNonce:
+    def test_if_there_is_a_redis_error_assume_nonce_has_been_seen(
+        self, app_with_mock_cache, logging
+    ):
         sender_id = 0
         nonce = 'asdf'
         timestamp = datetime.datetime(2019, 1, 1)
-        app.cache.get.side_effect = redis.exceptions.ConnectionError()
+        app_with_mock_cache.cache.get = unittest.mock.Mock()
+        app_with_mock_cache.cache.get.side_effect = redis.exceptions.ConnectionError()
         result = seen_nonce(sender_id, nonce, timestamp)
-        self.assertTrue(result)
+        assert result is True
         logging.error.assert_called_once()
 
 
