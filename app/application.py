357542429a291385d693355c15eb38019a4efecf
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2020-05-06 18:26:28 +0100

    Format using black

--- app/application.py
+++ app/application.py
@@ -3,7 +3,9 @@ import os
 from app.sso.register import register_sso_component
 
 config_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'config'))
-template_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'templates'))
+template_location = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__), 'templates')
+)
 static_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'static'))
 
 
