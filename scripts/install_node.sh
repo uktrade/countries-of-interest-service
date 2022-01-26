10a2db36833fa70caa67c46c2b1298b54d4f896b
Nick Ross <nick.ir.ross@gmail.com>
2022-01-26 14:00:21 +0000

    upgrade node and python to allow for iipython version bump

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -2,7 +2,7 @@
 
 source ./scripts/functions.sh
 
-run "curl https://deb.nodesource.com/setup_10.x --output node.sh"
+run "curl https://deb.nodesource.com/setup_12.x --output node.sh"
 run "bash node.sh -b"
 run "rm -f node.sh"
 run "apt-get install -y nodejs"
